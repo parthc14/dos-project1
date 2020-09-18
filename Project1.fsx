ea09d310e6628971db509eaf8e788c9f86b4cad7
parthc14 <parthchitroda@gmail.com>
2020-09-18 14:33:02 -0400

    Working on small input

--- Project1.fsx
+++ Project1.fsx
@@ -9,11 +9,7 @@ open Akka.Configuration
 open Akka.FSharp
 open Akka.TestKit
 
-// #Using Actor
-// Actors are one of Akka's concurrent models.
-// An Actor is a like a thread instance with a mailbox.
-// It can be created with system.ActorOf: use receive to get a message, and <! to send a message.
-// This example is an EchoServer which can receive messages then print them.
+
 
 let system = ActorSystem.Create("FSharp")
 
@@ -23,16 +19,12 @@ open Akka.Configuration
 open Akka.FSharp
 open Akka.TestKit
 
-// #Using Actor
-// Actors are one of Akka's concurrent models.
-// An Actor is a like a thread instance with a mailbox.
-// It can be created with system.ActorOf: use receive to get a message, and <! to send a message.
-// This example is an EchoServer which can receive messages then print them.
 
 type EchoServer(id, startNum, endNum, k) =
     inherit Actor()
         
     override x.OnReceive message =
+        
         for x in [startNum..endNum] do
             let y = x+k - 1
             let sum1 = (y *(y+1)  *((2*y)+1))                                    
@@ -42,39 +34,42 @@ type EchoServer(id, startNum, endNum, k) =
             let unwantedSum = unSum / 6 |>float
             let realSum =  sum - unwantedSum                                  
             let sqrRoot = sqrt realSum                                 
-            let roundSqrt = floor sqrRoot |> float                  
+            let roundSqrt =  sqrRoot |> floor |> float                  
             if sqrRoot-roundSqrt = 0.0 then                               
                 printfn "%i" x
 
         match message with
-        | :? string as msg ->  () //printfn "ID : %i startsNum %i endNum %i k %i " id startNum endNum k  
+        | :? string as msg -> () //printfn "Actor with startRange %i -  endRange %i"  startNum endNum  
         | _ ->  failwith "unknown message"
+
+
+
 
 let n = 40 |> float
 let k = 24 |> float
 let noOfCores = Environment.ProcessorCount |> float
 let noActors = 10.0 * noOfCores |> float
 let workRange =  n / noActors |> float |> ceil |>int
-
+// printfn "Number of Actors %f" noActors
+// printfn "Work Range %i" workRange
 
 
 let intActors = noActors |>int
-
 let allActors = 
-    [1 .. intActors-1]
+    [1 .. workRange-1]
     |> List.map(fun id ->   let properties = [|  int(id):> obj; int(0 + (id-1)* workRange + 1):>obj; int(0 + (id)*workRange) :> obj ;int(k) :> obj |]
                             system.ActorOf(Props(typedefof<EchoServer>, properties)))
             
             
 let finalActor = 
-    [intActors-1 .. intActors]
-    |> List.map(fun id ->   let properties = [|  int(intActors):>obj; int(0 + (intActors - 1)*workRange + 1) :> obj ;int(n) :> obj; (int)(k):> obj |]
+    [workRange-1 .. workRange]
+    |> List.map(fun id ->   let properties = [|  int(workRange):>obj; int(0 + (workRange - 1)*workRange + 1) :> obj ;int(n) :> obj; (int)(k):> obj |]
                             system.ActorOf(Props(typedefof<EchoServer>, properties)))
 
 let finalList = allActors @ finalActor   
 
 
-for id in [ 1 .. intActors] do
+for id in [ 1 .. workRange] do
     List.item (id) finalList
     <! sprintf "F# %d!" id
 
@@ -101,3 +96,4 @@ system.Terminate()
 
 // let lastList =  [|  int(intActors):>obj; int(0 + (intActors - 1)*workRange + 1) :> obj ;int(n) :> obj; (int)(k):> obj |]
 // let finalActor = system.ActorOf(Props(typedefof<EchoServer>, lastList))
+// 8576
